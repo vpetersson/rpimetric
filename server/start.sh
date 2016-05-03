e3c95557850ac2355dfff0f93bcbfebd990c1200
Viktor Petersson <vpetersson@wireload.net>
2016-05-03 23:24:35 +0100

    Minor tweaks.

--- server/start.sh
+++ server/start.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+if [ ! -f /usr/local/prometheus/prometheus.yml ]; then
+  cp server/prometheus.yml /usr/local/prometheus/
+fi
+
 # Start the API endpoint that
 # the collector talks to.
 docker run -d \
   --name pushgateway \
   -p 9091:9091 \
+  --restart=always \
   prom/pushgateway
 
 # Start the database
 docker run -d \
   --link pushgateway \
   --name prometheus \
+  --restart=always \
   -v /usr/local/prometheus:/prometheus \
   prom/prometheus -config.file=/prometheus/prometheus.yml
 
@@ -18,5 +24,6 @@ docker run -d \
 docker run -d \
   --name=grafana \
   --link prometheus \
+  --restart=always \
   -p 3000:3000 \
   grafana/grafana
